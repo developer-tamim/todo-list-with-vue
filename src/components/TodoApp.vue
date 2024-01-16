ec8272ed2385a5b4731a39d4cc340d6a4de81750
DevTeam <tawsiftamim24@gmail.com>
2024-01-16 22:07:38 +0600

    create and edit

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -19,18 +19,37 @@
           <thead>
             <tr>
               <th scope="col">Tasks</th>
-              <th scope="col" style="width:100px;">Status</th>
+              <th scope="col" style="width: 100px">Status</th>
               <th scope="col" class="text-center">Edit</th>
               <th scope="col" class="text-center">Delete</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>  
+            <tr v-for="(task, index) in tasks" :key="index">
+              <td>{{ task.tasks }}</td>
+              <td>
+                <span 
+                class="pointer select" 
+                @click="changeStatus(index)"
+                :class="{
+                  'text-danger' :task.status==='to-do',
+                  'text-success' :task.status==='in-progress',
+                  'text-warning' :task.status==='finished',
+                }"
+                >
+                </span>
+              </td>
+              <td class="text-center">
+                <div @click="editTask(index)">
+                  <i class="fa-regular fa-pen-to-square"></i>
+                </div>
+              </td>
+              <td class="text-center">
+                <div @click="deleteTask(index)">
+                  <i class="fa-solid fa-trash"></i>
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
@@ -39,9 +58,43 @@
 </template>
 
 <script>
-    export default {
-        name: "TodoApp",
+export default {
+  name: "TodoApp",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      task: "",
+      editTask: null,
+      status: ["to-do", "in-progress", "finished"],
+      tasks: [
+        {
+          task: "Tasks1",
+          status: "to-do",
+        },
+        {
+          task: "Tasks2",
+          status: "in-progress",
+        },
+        {
+          task: "Tasks3",
+          status: "finished",
+        },
+      ],
     };
+  },
+  methods:{
+    capitalizeFirstChart(srt){
+      return srt.charAt(0).toUpperCase() + srt.slice(1);
+    },
+    changeStatus(index){
+      let newIndex=this.statuses.indexOf(this.task[index].status);
+      if(++newIndex>2) newIndex=0;
+      this.task[index].status=this.statuses[newIndex];
+  }
+  }
+};
 </script>
 
 <style lang=""></style>
